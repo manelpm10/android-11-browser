71820a53e70fe1502352f91e56386230a3579953
Manel <yo@manelperez.com>
2018-06-13 11:14:18 +0200

    Added intent to suggest app when user try to open an url

--- app/src/main/java/es/pue/android/browser/MainActivity.java
+++ app/src/main/java/es/pue/android/browser/MainActivity.java
@@ -2,6 +2,7 @@ package es.pue.android.browser;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //WebView wbBrowser = new WebView(this, );
     }
 }
